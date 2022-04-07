4b701b07b9605df3dbfb0be869274f1a0da1e353
dengchuhan <dengchuhan@5ireport.com>
2022-04-07 16:01:45 +0800

    4.7Thinkpad提交store到本地仓库

---
+++ src/main/java/com/cy/store/controller/BaseController.java
@@ -0,0 +1,30 @@
+package com.cy.store.controller;
+
+/**
+ * @author dch
+ * @create 2022-04-07 15:06
+ */
+
+import com.cy.store.service.ex.InsertException;
+import com.cy.store.service.ex.ServiceException;
+import com.cy.store.service.ex.UsernameDuplicateException;
+import com.cy.store.util.JsonResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/** 控制器类的基类 */
+public class BaseController {
+    /** 操作成功的状态码 */
+    public static final int OK = 200;
+
+    /** @ExceptionHandler用于统一处理方法抛出的异常 */
+    @ExceptionHandler(ServiceException.class)
+    public JsonResult<Void> handleException(Throwable e) {
+        JsonResult<Void> result = new JsonResult<Void>(e);
+        if (e instanceof UsernameDuplicateException) {
+            result.setState(4000);
+        } else if (e instanceof InsertException) {
+            result.setState(5000);
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/cy/store/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.cy.store.controller;
+
+/**
+ * @author dch
+ * @create 2022-04-07 14:57
+ */
+
+
+import com.cy.store.entity.User;
+import com.cy.store.service.IUserService;
+import com.cy.store.util.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/** 处理用户相关请求的控制器类 */
+@RestController
+@RequestMapping("users")
+public class UserController extends BaseController {
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping("reg")
+    public JsonResult<Void> reg(User user) {
+        // 调用业务对象执行注册
+        userService.reg(user);
+        // 返回
+        return new JsonResult<Void>(OK);
+    }
+}
